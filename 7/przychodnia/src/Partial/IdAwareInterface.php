e0b89c02472e213018aba909e41efdc160c6cd91
Adam Michna <michnaadam33@gmail.com>
2019-10-06 08:41:43 +0200

    l7

---
+++ 7/przychodnia/src/Partial/IdAwareInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Partial;
+
+
+interface IdAwareInterface
+{
+    public function getId(): ?int;
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void;
+}
